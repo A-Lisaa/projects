e94754ac36b6126cb5f07409c981a8dbd00fc93e
Alice <alice.dvache17@gmail.com>
2021-10-22 20:35:47 +0500

    Wallpaper moved to its own repo, minor changes

--- nhentai_grabber.py
+++ nhentai_grabber.py
@@ -1,13 +1,13 @@
-#-*- coding: utf-8 -*-
-print("Preparing for parsing")
-
+# -*- coding: utf-8 -*-
 import os
 import time
 import requests
-from typing import Callable, Union
 from bs4 import BeautifulSoup
 
-"""
+__python_version__ = 3.10
+__doc__ = """
+! Minimum python version is 3.10
+
 Downloads manga and doujinsi from nhentai.net.
 Gets and downloads tags of a manga.
 
@@ -17,20 +17,24 @@ Usage:
 """
 # TODO: Problems may occur when manga name is too long
 
+print("Preparing for parsing")
 class Grabber:
+    """
+    Main class, see file docstring
+    """
     class Decorators:
         """
         Class for decorators
         """
         @classmethod
-        def connecter(cls, func: Callable):
+        def connecter(cls, func):
             """
             Tries to connect ot website
 
             Args:
                 func (Callable): function to retry
             """
-            def wrapper(self, *args, **kwargs):
+            def inner(self, *args, **kwargs):
                 try:
                     result = func(self, *args, **kwargs)
                 except requests.RequestException:
@@ -43,24 +47,26 @@ class Grabber:
                         except requests.RequestException:
                             if self.try_counts > 0:
                                 print(f"Error, trying again, left {try_counter} attempt(s)")
-                                
+
                                 start_time = time.time()
                                 if int(time.time() - start_time) < self.trying_time:
                                     time.sleep(self.trying_time - time.time() + start_time)
                             elif self.try_counts == 0:
                                 print("Error")
-                                
+
                                 result = None
                             elif self.try_counts < 0:
                                 print("Error, trying again")
-                                
+
                                 start_time = time.time()
                                 if int(time.time() - start_time) < self.trying_time:
                                     time.sleep(self.trying_time - time.time() + start_time)
                 return result
-            return wrapper
-    
-    def __init__(self, folder: str, try_counts: int = -1, trying_time: int = 3, tags_delimiter: str = "; ", tags_file: str = "tags.txt"):
+            return inner
+
+    def __init__(self, folder: str,
+                 try_counts: int = -1, trying_time: int = 3,
+                 tags_delimiter: str = "; ", tags_file: str = "tags.txt"):
         """
         Downloads doujinsis from nhentai.net
 
@@ -87,15 +93,16 @@ class Grabber:
         Returns:
             str: string with replaced denied marks
         """
-        denied_marks = {"/":"%2F", "\\":"%5C", "*":"%2A", ":":"%3A", "?":"%3F", "\"":"%22", "<":"%3C", ">":"%3E", "|":"%7C"}
-        
-        for mark in denied_marks:
-            string = string.replace(mark, denied_marks[mark])
-            
+        denied_marks = {"/":"%2F", "\\":"%5C", "*":"%2A", ":":"%3A",
+                        "?":"%3F", "\"":"%22", "<":"%3C", ">":"%3E", "|":"%7C"}
+
+        for mark, code in denied_marks.items():
+            string = string.replace(mark, code)
+
         return string
 
     @Decorators.connecter
-    def get_soup(self, link: str) -> Union[BeautifulSoup, None]:
+    def get_soup(self, link: str) -> BeautifulSoup | None:
         """
         Gets BeautifulSoup of a page
 
@@ -106,7 +113,7 @@ class Grabber:
             Union[BeautifulSoup, None]: BeautifulSoup if connecting is succesful, else None
         """
         return BeautifulSoup(requests.get(link).content, "html.parser")
-        
+
     def get_tags(self, soup: BeautifulSoup) -> dict:
         """
         Gets tags of a doujinsi
@@ -117,16 +124,17 @@ class Grabber:
         Returns:
             dict: dictionary with tags in format "Name":"%name%", "ID":"%id%", etc.
         """
-        tags = {"ID":"", "Name":"", "Parodies":"", "Characters":"", "Tags":"", "Artists":"", "Groups":"", "Languages":"", "Categories":"", "Pages":""}
-        
+        tags = {"ID":"", "Name":"", "Parodies":"", "Characters":"", "Tags":"",
+                "Artists":"", "Groups":"", "Languages":"", "Categories":"", "Pages":""}
+
         tags["Name"] += soup.select("h1.title")[0].text
         tags["ID"] += soup.select("#gallery_id")[0].text[1:] # From 1 because the first char is # (#123456)
         for tag_container in soup.select("div.tag-container")[:-1]: # To -1 because the last tag is Uploaded
             for name in tag_container.select("span.name"):
                 tags[tag_container.contents[0].strip()[:-1]] += name.text.strip() + self.tags_delimiter # To -1 because the last char is : (Pages:)
-        
+
         return tags
-                
+
     @Decorators.connecter
     def download_page(self, link: str, manga_folder: str):
         """
@@ -136,9 +144,9 @@ class Grabber:
             link (str): link to a page
             manga_folder (str): folder in which the page will be downloaded
         """
-        with open(f"{manga_folder}\\{link.split('/')[-1]}", "wb") as f:
-            f.write(requests.get(link).content)
-        
+        with open(f"{manga_folder}\\{link.split('/')[-1]}", "wb") as file:
+            file.write(requests.get(link).content)
+
     def manga_page_parser(self, link: str, first_page: int = 1, last_page: int = None):
         """
         Parses manga page and downloads pics and tags
@@ -155,26 +163,26 @@ class Grabber:
         manga_folder = f"{self.folder}\\{self.replace_denied_marks(tags['Name'])}"
         if not os.path.exists(manga_folder):
             os.makedirs(manga_folder)
-        
+
         # Downloads pages from first_page to last_page
         for img in soup.select("img.lazyload")[first_page:last_page+1]: # From 1 because of cover, to Pages+1 because of Thumbs
             data_src = img.attrs['data-src'].split('/')
             page, extension = os.path.splitext(data_src[-1])
             img_link = f"https://i.nhentai.net/galleries/{data_src[-2]}/{page[:-1]}{extension}"
-            
+
             self.download_page(img_link, manga_folder)
-            
+
             # Adds manga ID to the end of each picture code
-            with open(f"{manga_folder}\\{page[:-1]}{extension}", "a") as f:
-                f.write(f"\nID:{tags['ID']}")
-               
+            with open(f"{manga_folder}\\{page[:-1]}{extension}", "a", encoding = "utf-8") as file:
+                file.write(f"\nID:{tags['ID']}")
+
             print(f"{img_link} SAVED TO {manga_folder}")
-            
+
         # Writes tags to a file
-        with open(f"{manga_folder}\\{self.tags_file}", "w", encoding = "utf-8") as f:
-            for tag in tags:
-                f.write(f"{tag}: {tags.get(tag).strip()}\n")
-                
+        with open(f"{manga_folder}\\{self.tags_file}", "w", encoding = "utf-8") as file:
+            for tag, value in tags.items():
+                file.write(f"{tag}: {value.strip()}\n")
+
     def run(self, link: str):
         """
         Main method, call this with manga link as an argument.
@@ -186,5 +194,5 @@ class Grabber:
 
 if __name__ == "__main__":
     grabber = Grabber("F:\\H")
-    for manga in open("E:\\Secret Info\\Файлы\\Циферки.txt"):
+    for manga in open("E:\\Secret Info\\Файлы\\Циферки.txt", encoding = "utf-8"):
         grabber.run(manga.strip())
