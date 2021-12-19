5584aa35045463edeb92b63b7b0ecd1f0527e777
Alice <alice.dvache17@gmail.com>
2021-12-20 02:59:51 +0500

    xmas_tree and quadratic solver creation

--- quadratic_equation_solver.py
+++ quadratic_equation_solver.py
@@ -1,6 +1,10 @@
+import math
 import re
-from math import sqrt, gcd
-from typing import Generator
+from decimal import Decimal
+from typing import Generator, Iterable
+
+from rich.console import Console
+from rich.table import Table
 
 
 class Solver:
@@ -37,7 +41,23 @@ class Solver:
 
         return equation
 
-    def get_coefficient(self, pattern: str, equation: str) -> float:
+    def reduce_numbers(self, first_number: Decimal, second_number: Decimal) -> tuple[Decimal, Decimal]:
+        fraction_gcd = Decimal(math.gcd(int(first_number), int(second_number)))
+
+        return (first_number/fraction_gcd, second_number/fraction_gcd)
+
+    def get_reduced_equation(self, a: Decimal, b: Decimal, c: Decimal) -> str:
+        quadratic_coefficient = "x^2 " if a == 1 else "-x^2 " if a == -1 else f"{a}x^2 "
+
+        linear_coefficient_sign = "+" if b > 0 else "-"
+        linear_coefficient = f"{linear_coefficient_sign} {abs(b)}x " if b != 0 else ""
+
+        free_term_sign = "+" if c > 0 else "-"
+        free_term = f"{free_term_sign} {abs(c)} " if c != 0 else ""
+
+        return f"{quadratic_coefficient}{linear_coefficient}{free_term}= 0"
+
+    def get_coefficient(self, pattern: str, equation: str) -> Decimal:
         """Gets one and only one coefficient of an equation w/o [=] (for example: gets coefficient of a)
 
         Args:
@@ -45,21 +65,21 @@ class Solver:
             equation (str): equation with coefficients
 
         Returns:
-            float: sum of all coeficients in equation
+            Decimal: sum of all coeficients in equation
         """
         occurences = re.finditer(pattern, equation)
         coefficients = (re.search(r"[+-][+-]?(\d*\.)?\d+", occurence[0])[0] for occurence in occurences)
 
-        return sum((float(i) for i in coefficients))
+        return Decimal(sum((Decimal(i) for i in coefficients)))
 
-    def get_all_coefficients(self, equation: str) -> tuple[float, float, float]:
+    def get_all_coefficients(self, equation: str) -> tuple[Decimal, Decimal, Decimal]:
         """Gets all coefficients of equation w/o [=]
 
         Args:
             equation (str): equation from which coefficients are to be taken
 
         Returns:
-            tuple[float, float, float]: tuple containing (a, b, c) coefficients
+            tuple[Decimal, Decimal, Decimal]: tuple containing (a, b, c) coefficients
         """
         equation = self.normalize_equation(equation)
 
@@ -69,11 +89,11 @@ class Solver:
 
         return (a_coefficient, b_coefficient, c_coefficient)
 
-    def get_equation_coefficients(self, equation: str) -> Generator[float, None, None] | tuple[float, ...]:
+    def get_equation_coefficients(self, equation: str) -> Generator[Decimal, None, None] | tuple[Decimal, ...]:
         """Gets final coefficients of an equation
 
         Returns:
-            Generator[float, None, None] | tuple[float, ...]: generator object with (a, b, c) coefficients
+            Generator[Decimal, None, None] | tuple[Decimal, ...]: generator object with (a, b, c) coefficients
         """
         # We split equation by [=], we will work with both parts separately
         left_part, right_part = (part.strip() for part in equation.split("="))
@@ -85,103 +105,125 @@ class Solver:
 
         # We will divide all coefficients by one number if we will not fall in float numbers lake
         if all(float(i).is_integer() for i in final_coefficients):
-            coefficients_gcd = gcd(*(int(i) for i in final_coefficients))
+            coefficients_gcd = math.gcd(*(int(i) for i in final_coefficients))
             final_coefficients = (i / coefficients_gcd for i in final_coefficients)
 
         return final_coefficients
 
-    def get_equation_roots(self, discriminant: float, a: float, b: float) -> tuple[float | str, float | str]:
+    def get_equation_solution(self, a: Decimal, b: Decimal) -> str:
+        numerator, denominator = self.reduce_numbers(-b, (2*a)) if b != 0 else (-b, 2*a)
+        free_term = f"({a})" if a < 0 else str(a)
+
+        return  f"x = {-b} / (2*{free_term}) = {numerator} / {denominator} = {(numerator / denominator).normalize()}"
+
+    def get_equation_solutions(self, discriminant: Decimal, a: Decimal, b: Decimal) -> str:
         """Gets 2 roots of full quadratic equation
 
         Returns:
-            tuple[float | str, float | str]: tuple with (root1, root2)
+            str: tuple with (root1, root2)
         """
-        discriminant_sqrt = sqrt(discriminant)
-        # Altered discriminant sqrt is used is strings
-        altered_discriminant_sqrt = discriminant_sqrt
-        splitted_sqrt = [1, discriminant]
+        taking_out1 = taking_out2 = ""
+        reduced_taking_out1 = reduced_taking_out2 = ""
+        discriminant_sqrt = discriminant.sqrt()
+        free_term = f"({a})" if a < 0 else str(a)
+
+        numerator1, denominator1 = self.reduce_numbers((-b + discriminant_sqrt), (2*a))
+        numerator2, denominator2 = self.reduce_numbers((-b - discriminant_sqrt), (2*a))
+
+        letter_formula1 = "(-b + sqrt(discriminant)) / (2*a) =\n"
+        letter_formula2 = "(-b - sqrt(discriminant)) / (2*a) =\n"
+
+        full_formula1 = f" ({-b} + sqrt({discriminant})) / (2*{free_term}) =\n"
+        full_formula2 = f" ({-b} - sqrt({discriminant})) / (2*{free_term}) =\n"
+
+        linear_coefficient1 = f"{-b} + " if b != 0 else ""
+        linear_coefficient2 = f"{-b} - " if b != 0 else ""
 
         # We take out the biggest square root from discriminant, so it will be not sqrt(20), but 2*sqrt(5)
-        for i in (j*j for j in range(int(discriminant_sqrt), 1, -1)):
-            if discriminant % i == 0:
-                taken_out = sqrt(i)
-                decimated_root = discriminant/i
-                altered_discriminant_sqrt = f"{taken_out}*sqrt({decimated_root})"
-                splitted_sqrt = [taken_out, decimated_root]
+        if not float(discriminant_sqrt).is_integer():
+            for i in (Decimal(j*j) for j in range(int(discriminant_sqrt), 0, -1)):
+                if discriminant % i == 0:
+                    taken_out = i.sqrt()
+                    decimated_root = discriminant/i
 
-        if self.always_use_sqrt or (discriminant_sqrt * 10**str(discriminant)[::-1].find('.')).is_integer():
-            # We will get here only if we want to use sqrt function for all numbers (False by default) or sqrt of discriminant is rational
-            root1 = (-b + discriminant_sqrt) / (2*a)
-            root2 = (-b - discriminant_sqrt) / (2*a)
-        else:
-            numerator = altered_discriminant_sqrt
-            denominator = 2*a
+                    reduced_taken_out, reduced_denominator = self.reduce_numbers(taken_out, 2*a)
+                    denominator1 = denominator2 = reduced_denominator
 
-            if b != 0:
-                root1 = f"({-b} + {numerator}) / {denominator}"
-                root2 = f"({-b} - {numerator}) / {denominator}"
-                return (root1, root2)
+                    taking_out1 = f" ({linear_coefficient1}{taken_out}*sqrt({decimated_root})) / (2*{a}) =\n"
+                    taking_out2 = f" ({linear_coefficient2}{taken_out}*sqrt({decimated_root})) / (2*{a}) =\n"
 
-            # We divide numerator and denominator on greatest common divisor if we can
-            ratio_gcd = gcd(int(splitted_sqrt[0]), int(2*a))
-            if a.is_integer():
-                taken_out = splitted_sqrt[0] / ratio_gcd
-                if taken_out != 1.0:
-                    numerator = f"{taken_out}*sqrt({splitted_sqrt[1]})"
-                else:
-                    numerator = f"sqrt({splitted_sqrt[1]})"
+                    reduced_taking_out1 = f" ({linear_coefficient1}{reduced_taken_out}*sqrt({decimated_root})) / {reduced_denominator} =\n"
+                    reduced_taking_out2 = f" ({linear_coefficient2}{reduced_taken_out}*sqrt({decimated_root})) / {reduced_denominator} =\n"
 
-                new_denominator = denominator / ratio_gcd
-                if new_denominator != 1.0:
-                    denominator = new_denominator
-                else:
-                    denominator = 0
+                    root_extraction1 = f" ({linear_coefficient1}{discriminant_sqrt}) / {reduced_denominator} =\n"
+                    root_extraction2 = f" ({linear_coefficient2}{discriminant_sqrt}) / {reduced_denominator} =\n"
 
-            if denominator != 0:
-                root1 = f"({numerator}) / {denominator}"
-                root2 = f"(-{numerator}) / {denominator}"
-            else:
-                root1 = f"{numerator}"
-                root2 = f"-{numerator}"
+                    numerator_addition1 = f" {-b + discriminant_sqrt} / {reduced_denominator} =" if reduced_denominator != 0 else ""
+                    numerator_addition2 = f" {-b - discriminant_sqrt} / {reduced_denominator} =" if reduced_denominator != 0 else ""
 
-        return (root1, root2)
+                    break
 
-    def solve_quadratic_equation(self, equation: str) -> (tuple[float | str, float | str] | float | None):
+        root_extraction1 = f" ({linear_coefficient1}{discriminant_sqrt}) / (2*{a}) =\n"
+        root_extraction2 = f" ({linear_coefficient2}{discriminant_sqrt}) / (2*{a}) =\n"
+
+        numerator_addition1 = f" {-b + discriminant_sqrt} / {2*a} =\n"
+        numerator_addition2 = f" {-b - discriminant_sqrt} / {2*a} =\n"
+
+        dividing1 =  f" {numerator1} / {denominator1} =\n"
+        dividing2 =  f" {numerator2} / {denominator2} =\n"
+
+        result1 = f" {(numerator1 / denominator1).normalize()}"
+        result2 = f" {(numerator2 / denominator2).normalize()}"
+
+        expression1 = f"x1 = {letter_formula1}{full_formula1}{taking_out1}{reduced_taking_out1}{root_extraction1}{numerator_addition1}{dividing1}{result1}"
+        expression2 = f"x2 = {letter_formula2}{full_formula2}{taking_out2}{reduced_taking_out2}{root_extraction2}{numerator_addition2}{dividing2}{result2}"
+
+        return f"{expression1}\n\n{expression2}"
+
+    def solve_quadratic_equation(self, equation: str) -> (tuple[str, str, str]):
         """Solves quadratic equation and returns roots
 
         Args:
             equation (str): quadratic equation to solve
 
         Returns:
-            tuple[float | str, float | str] | float | None]: tuple containing roots if there are 2 of them (roots can be represented as strings if they are irrational), 1 number if there is only one root and None if there is no real solutions
+            tuple[str, str, str]: tuple containing roots if there are 2 of them (roots can be represented as strings if they are irrational), 1 number if there is only one root and None if there is no real solutions
         """
         a, b, c = self.get_equation_coefficients(equation)
+        reduced_equation = self.get_reduced_equation(a, b, c)
         discriminant = b*b - 4*a*c
 
         if discriminant < 0:
-            return None
-        if discriminant == 0:
-            return -b / (2*a)
-        return self.get_equation_roots(discriminant, a, b)
+            solution = "No real roots"
+        elif discriminant == 0:
+            solution = self.get_equation_solution(a, b)
+        else:
+            solution = self.get_equation_solutions(discriminant, a, b)
 
-    def print_solution(self, equation: str):
+        return (reduced_equation, f"D = {b}^2 - 4*{a}*{c} = {b*b} - {4*a*c} = {discriminant}", solution)
+
+
+    def print_solution(self, equations: Iterable):
         """Pretty prints solution to an equation
 
         Args:
             equation (str): equation to solve and print
         """
-        result = self.solve_quadratic_equation(equation)
-        if isinstance(result, tuple):
-            print(f"{equation.strip()}: {result[0]}; {result[1]}")
-        elif isinstance(result, float):
-            print(f"{equation.strip()}: {result}")
-        elif result is None:
-            print(f"{equation.strip()}: No real roots")
+        console = Console()
+        table = Table(title="Equations solutions")
+        table.add_column("Equation", justify="center", style="red")
+        table.add_column("Reduced equation", justify="center", style="green")
+        table.add_column("Discriminant", justify="center", style="cyan")
+        table.add_column("Solution", justify="center", style="green")
+
+        for equation in equations:
+            reduced_equation, discriminant, solution = self.solve_quadratic_equation(equation)
+            table.add_row(equation, reduced_equation, discriminant, solution, end_section=True)
+
+        console.print(table)
 
 
 if __name__ == "__main__":
     solver = Solver()
-    with open("./quadratic_equations.txt", encoding="utf-8") as file:
-        for line in file:
-            solver.print_solution(line)
-    #solver.print_solution("5х2-25 = 0")
+    solver.print_solution((line for line in open("./quadratic_equations.txt", encoding="utf-8")))
+    #solver.print_solution(("5х2-25 = 0",))
---
+++ test4.py
@@ -0,0 +1,4 @@
+a = range(5)
+print(a)
+a = a[::-1]
+print(a)
